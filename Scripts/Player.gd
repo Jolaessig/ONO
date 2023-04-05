14c666f72447afa85458d9b6cd653e80e79cb609
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-05 13:54:25 +0200

    :(

---
+++ Scripts/Player.gd
@@ -0,0 +1,29 @@
+
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		animated_sprite.animation = "jump"
+	else:
+		if(velocity.x == 0):
+			animated_sprite.animation = "idle"
+		else:
+			animated_sprite.animation = "run"
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		move_and_slide()
