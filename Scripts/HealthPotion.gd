748b9272cabb0ed79ef5a12d0c66a108d8020ea6
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-09 19:36:33 +0200

    Potion Sound added

--- Scripts/HealthPotion.gd
+++ Scripts/HealthPotion.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 		
 		
 func _ready():
@@ -11,6 +12,7 @@ func _ready():
 func _on_body_entered(body):
 	# Free the coin node
 	$AnimationPlayer.play("bounce")
+	audio_player.play()
 	
 func _on_animation_player_animation_finished(anim_name):
 	queue_free()
