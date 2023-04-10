333f77afc9932062b81830e5fe1aff74b5f47f2e
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-10 17:47:16 +0200

    Bat flips now

--- Scripts/Bat.gd
+++ Scripts/Bat.gd
@@ -10,16 +10,5 @@ func _ready():
 	animated_sprite = get_node("BatAnimation")
 	animated_sprite.play("Bat")
 
-func _physics_process(delta):
-	# Calculate the direction the enemy should move
-	var direction = Vector2.RIGHT.rotated(rotation)
-	velocity = direction * SPEED
-
-	# Set the animation flip_h property based on the direction of movement
-	if velocity.x < 0:
-		animated_sprite.flip_h = true
-	elif velocity.x > 0:
-		animated_sprite.flip_h = false
-
 
 
