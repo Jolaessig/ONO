0dd97f842b135515e0956f628befd078d0d068b8
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-10 21:33:33 +0200

    run jump fix

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,8 +3,9 @@ class_name Player
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -320.0
-const RUNNING_JUMP_VELOCITY = -360.0
+const RUNNING_JUMP_VELOCITY = -250
 const FRICTION_FORCE = 100.0
+const RUNNING_TIME_REQUIRED = 0.3
 
 const ANIM_IDLE = "idle"
 const ANIM_RUN = "run"
@@ -13,20 +14,21 @@ const ANIM_JUMP = "jump"
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping = false
 var is_on_ground = false
+var running_time = 0.0
+var can_run_jump = false
 
 var coins = 0
-
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jumpSound = $Jump
 @onready var Global = get_node("res://Global.gd")
 
 func _physics_process(delta: float) -> void:
-	
+
 	var speed = SPEED
 	var jump_velocity = JUMP_VELOCITY
 	
-	if Input.is_action_pressed("ui_x") and is_on_ground:
+	if Input.is_action_pressed("ui_x") and is_on_ground and can_run_jump:
 		speed *= 1.5
 		jump_velocity = RUNNING_JUMP_VELOCITY
 		
@@ -45,6 +47,14 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.flip_h = true
 			$AnimatedSprite2D.play("run")
 			
+		if Input.is_action_pressed("ui_x") and is_on_ground:
+			running_time += delta
+			if running_time >= RUNNING_TIME_REQUIRED:
+				can_run_jump = true
+		else:
+			running_time = 0.0
+			can_run_jump = false
+			
 	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	if direction != 0:
 		velocity.x = direction * speed
@@ -57,7 +67,10 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, speed * delta)
 			
 	if Input.is_action_just_pressed("ui_accept") and is_on_ground:
-		velocity.y = jump_velocity
+		if can_run_jump:
+			velocity.y = jump_velocity * 1.5
+		else:
+			velocity.y = jump_velocity
 		is_jumping = true
 		jumpSound.play()
 		
