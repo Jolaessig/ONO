8b1b977eb165f74ec3f3161a6e020dc61d0280d1
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-16 15:09:37 +0200

    slime enemy

---
+++ Slime.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	# Get the AnimatedSprite2D node
+	animated_sprite = get_node("SlimeAnimation")
+	animated_sprite.play("Slime")
