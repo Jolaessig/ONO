1f846ca33c4497d9fab345016c6a930c1aca35c1
Johannes <127427320+Jolaessig@users.noreply.github.com>
2023-04-18 19:57:38 +0200

    everthing killable

--- Scripts/GetHit2.gd
+++ Scripts/GetHit2.gd
@@ -3,8 +3,8 @@ extends Node2D
 @onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_area_entered(scene):
-	audio_player.play()
 	scene.get_parent().queue_free()
+	audio_player.play()
 	set_modulate(Color(1,0.3,0.3,1))
 	$Timer.start()
 	
